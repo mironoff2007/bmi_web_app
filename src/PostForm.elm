4ac18ad44e1ca8f9d7fd5472dd67f3f7de604d58
yumironov <mironoff2007@rambler.ru>
2019-03-26 10:26:11 +0300

    Restore lost js script

--- src/PostForm.elm
+++ src/PostForm.elm
@@ -131,7 +131,7 @@ update msg model =
             Err _ ->(model, Cmd.none)
     PostIt -> case validate modelValidator model of
                 Ok value -> ( {model| error=""},post model)
-                Err value -> ( {model| error=D.errorToString value}, Cmd.none)
+                Err value -> ( {model| error=Debug.toString value}, Cmd.none)
 
 --TODO wrap in forms
 view : Model -> Html Msg
